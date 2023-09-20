7ce3f818a6dfd823c76fc236142c31b50f947602
tigawanna <denniskinuthiaw@gmail.com>
2023-09-20 20:11:05 +0300

    add daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,21 @@
 import { type Config } from "tailwindcss";
 
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("daisyui"),
+    require("tailwindcss-animate"),
+    require("tailwind-scrollbar"),
+    require("tailwindcss-elevation"),
+  ],
+  // daisyUI config (optional - here are the default values)
+  daisyui: {
+    themes: ["cupcake", "luxury", "light", "lofi", "bumblebee"],
+  },
 } satisfies Config;
